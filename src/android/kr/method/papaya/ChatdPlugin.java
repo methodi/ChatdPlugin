fe743eb92400f643bc86dd4281eaa7c1ea8b3dcb
methodi <method@methodinc.co.kr>
2015-02-23 07:29:01 +0900

    Update ChatdPlugin.java

--- src/android/kr/method/papaya/ChatdPlugin.java
+++ src/android/kr/method/papaya/ChatdPlugin.java
@@ -7,16 +7,22 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.widget.Toast;
 
 public class ChatdPlugin extends CordovaPlugin {
 	private final String START_SERVICE = "start";
 	private final String STOP_SERVICE = "stop";
+	private final String CHATD_SEND = "send";
 	private BroadcastReceiver receiver;
 	private Intent intentMyService;
     private Context context;
+    private ChatdService mBoundService;
 
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 		//서비스 시작
@@ -25,6 +31,9 @@ public class ChatdPlugin extends CordovaPlugin {
 		//서비스 종료
 		} else if (action.equals(STOP_SERVICE)){
 			this.stopService();
+		} else if (action.equals(CHATD_SEND)){
+			String message = args.getString(0);
+			this.send(message);
 		}
 		
 		return false;
@@ -34,6 +43,9 @@ public class ChatdPlugin extends CordovaPlugin {
 	public void onDestroy(){LOG.d("ChatdPlugin", "OnDestroy");
 		if(context!=null && receiver!=null){
 			context.unregisterReceiver(receiver);
+		}
+		if(mBoundService!=null){
+			mBoundService.unbindPlugin();
 		}
 		super.onDestroy();
 	}
@@ -52,7 +64,8 @@ public class ChatdPlugin extends CordovaPlugin {
             IntentFilter mainFilter = new IntentFilter("kr.method.papaya.USER_ACTION");
             
             context.registerReceiver(receiver, mainFilter);
-            context.startService(intentMyService);
+            //context.startService(intentMyService);
+            context.bindService(intentMyService, mConnection, Context.BIND_AUTO_CREATE);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -64,4 +77,39 @@ public class ChatdPlugin extends CordovaPlugin {
 	private void stopService(){
 		
 	}
+	
+	public void send(String message){
+		if(mBoundService!=null){
+			mBoundService.send(message);
+		}
+	}
+	
+	public void onMessage(int action,String message){
+		switch(action){
+		case ChatdWebsocketClient.OPEN:
+			
+		break;
+		case ChatdWebsocketClient.CLOSE:
+			
+		break;
+		case ChatdWebsocketClient.MESSAGE:
+			
+		break;
+		case ChatdWebsocketClient.ERROR:
+			
+		break;
+		}
+	}
+	
+	private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            mBoundService = ((ChatdService.LocalBinder)service).getService();
+            mBoundService.bindPlugin(ChatdPlugin.this);
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+        	mBoundService.unbindPlugin();
+            mBoundService = null;
+        }
+    };
 }
